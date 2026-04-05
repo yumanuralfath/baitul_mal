c791cb0c938d78a3037b3796bc0c6c56e1c813dc
yumanuralfath <yumanuralfath2@gmail.com>
2026-04-05 17:27:32 +0700

    feat(title): change app name

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const BaitulMalApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class BaitulMalApp extends StatelessWidget {
+  const BaitulMalApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+      title: "Baitul Mal Plus",
+      home: Scaffold(body: Center(child: Text('Hello World'))),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
