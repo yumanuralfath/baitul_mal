5fe3813d3469237682599e6c68b0f1af3c84e65b
yumanuralfath <yumanuralfath2@gmail.com>
2026-04-08 09:00:49 +0700

    feat(modal): add show modal bottom when add project

--- lib/presentation/core/widget/action_button.dart
+++ lib/presentation/core/widget/action_button.dart
@@ -6,9 +6,64 @@ class ActionButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton.extended(
-      onPressed: () {},
+      onPressed: () {
+        _showAddProject(context);
+      },
       icon: const Icon(Icons.add),
       label: const Text("Tambah Project"),
     );
   }
+}
+
+void _showAddProject(BuildContext context) {
+  showModalBottomSheet(
+    context: context,
+    shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+    ),
+    builder: (context) => Padding(
+      padding: EdgeInsets.only(
+        bottom: MediaQuery.of(context).viewInsets.bottom,
+      ),
+      child: SingleChildScrollView(
+        //for content save at small screen
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "Tambah Project Baru",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                autofocus: true,
+                decoration: InputDecoration(
+                  labelText: "Nama Project",
+                  hintText: "Contoh: Tabungan Tahun 2026",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  filled: true,
+                ),
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                width: double.infinity, // fill button width modal size
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text("Simpan Project"),
+                ),
+              ),
+              const SizedBox(height: 10), //add aditional at bottom
+            ],
+          ),
+        ),
+      ),
+    ),
+    isScrollControlled: true,
+  ); // for keyboard not overlap with input
 }
